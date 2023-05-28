a63452547cc42906bc4261016253932b9b9aaa19
EkaterynaRuslanovna <gorgona3005@gmail.com>
2023-05-28 10:04:08 +0300

    Added control work

--- html_parsing_links/argparse_web.py
+++ html_parsing_links/argparse_web.py
@@ -2,7 +2,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Парсінг сайту")
 
-parser.add_argument("--url", type=str, required=False, help="Аргумент URL, за замовчуванням http://www.google.com")
+parser.add_argument("--url", type=str, required=False, help="Аргумент URL, наприклад: http://www.google.com")
 parser.add_argument("--endpoint", type=str, required=False, help="Ендпоінт, наприклад: '/search'")
 parser.add_argument("--params", type=str, required=False, help="Параметри пошуку (пошуковий запит(JSON format) наприклад: {\"q\": \"Hillel\"})")
 
